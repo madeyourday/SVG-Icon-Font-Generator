6cf1f9687d411889a61e4cc5f19be884643d69fa
Martin Auswöger <martin@auswoeger.com>
2015-10-29 10:38:52 +0100

    Use composer autoloading, fixes #4

--- run.php
+++ run.php
@@ -6,17 +6,7 @@
  * file that was distributed with this source code.
  */
 
-require __DIR__.'/vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-
-$loader = new Symfony\Component\ClassLoader\UniversalClassLoader;
-
-$loader->registerNamespaces(array(
-	'Symfony\\Component\\ClassLoader' => __DIR__.'/vendor/symfony/class-loader',
-	'Symfony\\Component\\Console' => __DIR__.'/vendor/symfony/console',
-	'MadeYourDay' => __DIR__.'/src',
-));
-
-$loader->register();
+require __DIR__.'/vendor/autoload.php';
 
 $cli = new MadeYourDay\SVG\IconFontGeneratorCLI;
 $cli->run();
